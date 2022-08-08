5c909c83a36e01059299eef490d86bc4eead0b44
NB2010NB <natanbalai1@gmail.com>
2022-08-08 14:00:39 +0300

    new

---
+++ main.ts
@@ -0,0 +1,60 @@
+//*******1******* */
+// let numUser: HTMLElement = document.getElementById('userInput');
+// function userType(): void {
+//   if (numUser.value> 10) {
+//     console.log('greater then 10');
+//   } else {
+//     console.log('less then 10');
+//   }
+// }
+// //*****2***** */
+// let num: HTMLElement = document.getElementById('inputUser');
+// function typeUser(): void {
+//   if (num.value > 10) {
+//     console.log('greater then 10');
+//   } else if (num.value < 10) {
+//     console.log('less then 10');
+//   } else {
+//     console.log('equal to 10');
+//   }
+// }
+// //******3****** */
+// let second: HTMLElement = document.getElementById('inputSecond');
+// let first: HTMLElement = document.getElementById('inputFirst');
+// let big: string;
+// let small: string;
+// function userTypes(): void {
+//   if (second < first) {
+//     console.log(first, '=big');
+//     console.log(second, '=small');
+//   } else if (second > first) {
+//     console.log(second, '=big');
+//     console.log(first, '=small');
+//   } else {
+//     console.log('values are equal');
+//   }
+// }
+// //*******4****** */
+// let num1:HTMLElement = document.getElementById("input1");
+// let num2:HTMLElement = document.getElementById("input2");
+// function minInput():HTMLElement{
+//  if(num1<num2){
+//    return num1
+//  }
+//  else{
+//    return num2
+//  }
+
+// }
+
+
+let num: HTMLElement = document.getElementById('userInput');
+function printUse(): void {
+  if (num > 10) { 
+    console.log("greaterThen10");
+  }
+  else{
+    console.log("lessThen10");
+    
+  }
+}
